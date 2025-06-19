1fe37696a5b8feba8c1507ea2aa94207bec50001
iam-vihaan <asathyavihaan@gmail.com>
2025-06-19 15:51:38 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "The AWS region to deploy in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket for frontend hosting"
+  type        = string
+  default     = "hello-world-frontend-bucket"
+}
+
+variable "api_name" {
+  description = "The name of the API Gateway"
+  type        = string
+  default     = "HelloWorldAPI"
+}
+
+variable "description" {
+  description = "Description of the API Gateway"
+  type        = string
+  default     = "API for Hello World Lambda"
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}

eb39c542e81375e7d30e2ded76dc1ce1406c99b3
iam-vihaan <asathyavihaan@gmail.com>
2025-06-19 17:01:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -105,7 +105,6 @@ resource "aws_api_gateway_integration" "lambda_integration" {
 resource "aws_api_gateway_deployment" "hello_deploy" {
   depends_on  = [aws_api_gateway_integration.lambda_integration]
   rest_api_id = aws_api_gateway_rest_api.hello_api.id
-  stage_name  = "prod"
 }
 
 # Cognito SSO

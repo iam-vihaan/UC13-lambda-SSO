c349aa6e4af1767e191a720dae05fed96a1dada7
iam-vihaan <asathyavihaan@gmail.com>
2025-06-20 14:23:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
 
 resource "aws_s3_bucket_public_access_block" "frontend_bucket_block" {
   bucket                  = aws_s3_bucket.kasi-hcl-bucket-uc13.id
-  block_public_acls       = true  
-  ignore_public_acls      = true   
+  block_public_acls       = false
+  ignore_public_acls      = false
   block_public_policy     = false
   restrict_public_buckets = false
 }
